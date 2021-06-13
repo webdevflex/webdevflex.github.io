3bb378b6b51d1cb787c37a3b0069e6a86c823f6c
webdevflex <58491237+webdevflex@users.noreply.github.com>
2021-06-14 00:33:19 +0300

    Add files via upload
    xzxz

---
+++ COURSE-TEST-WORK/src/js/script.js
@@ -0,0 +1,31 @@
+
+let sidebarHiden = document.querySelector('.side-bar');
+
+sidebarHiden.addEventListener('click',hidenBar);
+
+function hidenBar(){
+    sidebarHiden.classList.toggle('side-bar-hiden');
+}
+
+//hiden text
+let courceTextbtn = document.querySelector('.down-arrow-img');
+let courseText = document.querySelector('.info-course-subtitle')
+
+courceTextbtn.addEventListener('click',hidenCourseText);
+
+function hidenCourseText(){
+    courseText.classList.toggle('course-text-hiden');
+}
+
+//slider
+$('.info-course-slider').slick({
+    // arrows:false
+    responsive: [
+        {
+          breakpoint: 767,
+          settings: {
+            arrows:false
+          }
+        }
+    ]
+});
