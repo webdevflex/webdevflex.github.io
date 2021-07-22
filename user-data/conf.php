34152424419294cc93d9689e5f37bf0cfa76bf45
webdevflex <58491237+webdevflex@users.noreply.github.com>
2021-07-22 20:50:01 +0300

    Delete user-data directory

--- user-data/conf.php
+++
@@ -1,8 +0,0 @@
-<?
-$arr=array( // массив данных для подключения к базе данных
-	'dbhost'=>'localhost',
-	'dbname'=>'saveeyel_rootphp',
-	'dblogin'=>'saveeyel_admin',
-	'dbpass'=>'zapadlo453'
-);
-$conf=(object)$arr;
--- user-data/db.php
+++
@@ -1,8 +0,0 @@
-<?
-include __DIR__.'/conf.php'; // подключение файла конфигурации
-try{ // отлавливаем ошибку подключения
-	$_connect_=new pdo("mysql:host=$conf->dbhost;dbname=$conf->dbname;","$conf->dblogin","$conf->dbpass"); // создание подключения к базе данных
-	$_connect_->exec("set names utf8mb4"); // указание кодировки данных
-}catch(exception $error){
-	exit("Failed to get: ".$error->getMessage().':'.$error->getLine()); // вывод ошибки подключения к базе данных
-}
